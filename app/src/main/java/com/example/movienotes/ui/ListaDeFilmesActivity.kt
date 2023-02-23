85e34e7ad80347a930e2f4eb6f79ef43a9320844
phtrebil <phptrebil@gmail.com>
2023-02-22 21:21:10 -0300

    criando novo layout

--- app/src/main/java/com/example/movienotes/ui/ListaDeFilmesActivity.kt
+++ app/src/main/java/com/example/movienotes/ui/ListaDeFilmesActivity.kt
@@ -1,11 +1,12 @@
-package com.example.movienotes
+package com.example.movienotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movienotes.R
 
-class MainActivity : AppCompatActivity() {
+class ListaDeFilmesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lista_de_filmes)
     }
 }
