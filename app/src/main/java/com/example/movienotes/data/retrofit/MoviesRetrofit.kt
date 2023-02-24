9e7d6d5651c3e319a3d2cdf245e3f3ca044397cb
phtrebil <phptrebil@gmail.com>
2023-02-23 22:46:58 -0300

    retrofit

---
+++ app/src/main/java/com/example/movienotes/data/retrofit/MoviesRetrofit.kt
@@ -0,0 +1,33 @@
+package com.example.movienotes.data.retrofit
+
+import com.example.movienotes.model.Movies
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+class MoviesRetrofit {
+
+    val resposta = Retrofit.Builder()
+        .baseUrl("https://mdblist.p.rapidapi.com/?i=tt0073195")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val despensaService = resposta.create(BuscaMovies::class.java)
+}
+
+interface BuscaMovies{
+    @Headers(
+        "Accept: application/json",
+
+        "X-RapidAPI-Key: x",
+        "X-RapidAPI-Host: x"
+    )
+    @GET("?s={nome_movie}")
+    fun buscaTodas(
+        @Path("nome_movie") movie_nome: String
+    ): Call<List<Movies>>
+}
---
+++ app/src/main/java/com/example/movienotes/model/Movies.kt
@@ -0,0 +1,10 @@
+package com.example.movienotes.model
+
+class Movies(
+    private val id: String,
+    private val titulo: String,
+    private val imagem: String,
+    private val ano: String
+) {
+
+}
